94ceaea3b74cb1b1a432ab4474094cf14ce98fb2
Mario <marioptrova@gmail.com>
2021-04-07 21:16:39 -0500

    query

--- src/components/TagCard/index.jsx
+++ src/components/TagCard/index.jsx
@@ -5,32 +5,14 @@ import Utils from '../../utils/pageUtils';
 import style from './tags.module.less';
 
 const TagCard = (props) => {
-  const {
-    img, name, description, color,
-  } = props;
-  const tagPage = Config.pages.tag;
+  // const {
+  //   img, name, description, color,
+  // } = props;
+  // const tagPage = Config.pages.tag;
   return (
-    <Link className={style.tagCard} to={Utils.resolvePageUrl(tagPage, name)}>
-      <div className={style.tagCard}>
-        <div
-          className={style.tagImg}
-          style={{
-            backgroundImage: `url(${img})`,
-          }}
-        />
-        <div className={style.pd20px}>
-          <div className="textCenter">
-            <h4 style={{ color: `${color}` }}>
-              #
-              {name}
-            </h4>
-          </div>
-          <p>
-            {description}
-          </p>
-        </div>
-      </div>
-    </Link>
+   <>
+   null
+   </>
   );
 };
 
